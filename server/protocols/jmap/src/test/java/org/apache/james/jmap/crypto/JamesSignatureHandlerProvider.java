rouazana/james

Issue-15 Introduce Signature utils

protocols/jmap/src/test/java/org/apache/james/jmap/crypto/JamesSignatureHandlerProvider.java
@@ -1,0 +1,68 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.jmap.crypto;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.configuration.HierarchicalConfiguration;
+import org.apache.james.filesystem.api.FileSystem;
+import org.apache.james.protocols.lib.KeystoreLoader;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class JamesSignatureHandlerProvider {
+
+    public JamesSignatureHandler provide() throws Exception {
+        FileSystem fileSystem = new FileSystem() {
+            @Override
+            public InputStream getResource(String url) throws IOException {
+                return ClassLoader.getSystemResourceAsStream("keystore");
+            }
+
+            @Override
+            public File getFile(String fileURL) throws FileNotFoundException {
+                return null;
+            }
+
+            @Override
+            public File getBasedir() throws FileNotFoundException {
+                return null;
+            }
+        };
+        KeystoreLoader keystoreLoader = new KeystoreLoader();
+        keystoreLoader.setFileSystem(fileSystem);
+        JamesSignatureHandler signatureHandler = new JamesSignatureHandler(keystoreLoader);
+        signatureHandler.configure(createTestCConfiguration());
+        signatureHandler.init();
+        return signatureHandler;
+    }
+
+    private HierarchicalConfiguration createTestCConfiguration() {
+        HierarchicalConfiguration configuration = new HierarchicalConfiguration();
+        HierarchicalConfiguration.Node secretNode = new HierarchicalConfiguration.Node();
+        secretNode.setName("secret");
+        secretNode.setValue("james72laBalle");
+        configuration.addNodes("tls", Lists.newArrayList(secretNode));
+        return configuration;
+    }
+
+}

mbaechler/james-project

server/protocols/jmap/src/test/java/org/apache/james/jmap/crypto/JamesSignatureHandlerProvider.java
@@ -1,0 +1,66 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.jmap.crypto;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.configuration.HierarchicalConfiguration;
+import org.apache.james.filesystem.api.FileSystem;
+
+import com.google.common.collect.Lists;
+
+public class JamesSignatureHandlerProvider {
+
+    public JamesSignatureHandler provide() throws Exception {
+        FileSystem fileSystem = new FileSystem() {
+            @Override
+            public InputStream getResource(String url) throws IOException {
+                return ClassLoader.getSystemResourceAsStream("keystore");
+            }
+
+            @Override
+            public File getFile(String fileURL) throws FileNotFoundException {
+                return null;
+            }
+
+            @Override
+            public File getBasedir() throws FileNotFoundException {
+                return null;
+            }
+        };
+        JamesSignatureHandler signatureHandler = new JamesSignatureHandler(fileSystem);
+        signatureHandler.configure(createTestCConfiguration());
+        signatureHandler.init();
+        return signatureHandler;
+    }
+
+    private HierarchicalConfiguration createTestCConfiguration() {
+        HierarchicalConfiguration configuration = new HierarchicalConfiguration();
+        HierarchicalConfiguration.Node secretNode = new HierarchicalConfiguration.Node();
+        secretNode.setName("secret");
+        secretNode.setValue("james72laBalle");
+        configuration.addNodes("tls", Lists.newArrayList(secretNode));
+        return configuration;
+    }
+
+}