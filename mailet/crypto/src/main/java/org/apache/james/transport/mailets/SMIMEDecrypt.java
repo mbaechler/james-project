fba0863f18d473e9782f6de8814dfe5c92316fd0
Matthieu Baechler <matthieu@apache.org>
2017-12-22 16:54:51 +0100

    control flow should use blocks

--- mailet/crypto/src/main/java/org/apache/james/transport/mailets/SMIMEDecrypt.java
+++ mailet/crypto/src/main/java/org/apache/james/transport/mailets/SMIMEDecrypt.java
@@ -91,7 +91,9 @@ public class SMIMEDecrypt extends GenericMailet {
         String privateStoreType = config.getInitParameter("keyStoreType");
         
         String privateStoreFile = config.getInitParameter("keyStoreFileName");
-        if (privateStoreFile == null) throw new MessagingException("No keyStoreFileName specified");
+        if (privateStoreFile == null) {
+            throw new MessagingException("No keyStoreFileName specified");
+        }
         
         String privateStorePass = config.getInitParameter("keyStorePassword");
         
@@ -99,7 +101,9 @@ public class SMIMEDecrypt extends GenericMailet {
         String keyPass = config.getInitParameter("keyAliasPassword");
         
         String mailAttributeConf = config.getInitParameter("mailAttribute");
-        if (mailAttributeConf != null) mailAttribute = mailAttributeConf;
+        if (mailAttributeConf != null) {
+            mailAttribute = mailAttributeConf;
+        }
         
         try {
             keyHolder = new SMIMEKeyHolder(privateStoreFile, privateStorePass, keyAlias, keyPass, privateStoreType);
