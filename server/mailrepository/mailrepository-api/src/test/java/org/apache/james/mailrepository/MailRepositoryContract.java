2c02f61d860e92062056dd721c6e1bb3ec211864
benwa <btellier@linagora.com>
2018-01-18 15:12:27 +0700

    fixup! JAMES-2290 try to make MemoryMailRepository to break with parallelism (fail)

--- server/mailrepository/mailrepository-api/src/test/java/org/apache/james/mailrepository/MailRepositoryContract.java
+++ server/mailrepository/mailrepository-api/src/test/java/org/apache/james/mailrepository/MailRepositoryContract.java
@@ -25,10 +25,10 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
-import java.util.function.Function;
+import java.util.concurrent.TimeUnit;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -38,20 +38,18 @@ import org.apache.james.core.builder.MimeMessageBuilder;
 import org.apache.james.junit.ExecutorExtension;
 import org.apache.james.mailrepository.api.MailRepository;
 import org.apache.james.server.core.MailImpl;
-import org.apache.james.utils.Partition;
+import org.apache.james.util.concurrency.ConcurrentTestRunner;
+import org.apache.james.utils.DiscreteDistribution;
+import org.apache.james.utils.DiscreteDistribution.DistributionEntry;
 import org.apache.mailet.Mail;
 import org.apache.mailet.PerRecipientHeaders;
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
+import com.github.fge.lambdas.runnable.ThrowingRunnable;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMultimap;
 import com.google.common.hash.Hashing;
-import io.vavr.CheckedRunnable;
-import io.vavr.collection.Seq;
-import io.vavr.concurrent.Future;
 
 @ExtendWith(ExecutorExtension.class)
 public interface MailRepositoryContract {
@@ -321,37 +319,37 @@ public interface MailRepositoryContract {
     @Test
     default void storingAndRemovingMessagesConcurrentlyShouldLeadToConsistentResult(ExecutorService executorService) throws Exception {
         MailRepository testee = retrieveRepository();
-
+        int nbKeys = 2;
+        int nbIterations = 1000;
+        int threadCount = 20;
         ConcurrentHashMap.KeySetView<String, Boolean> expectedResult = ConcurrentHashMap.newKeySet();
 
-        int nbKeys = 200;
-        Function<Integer, CheckedRunnable> add = (Integer i) -> () -> {
-            String key = computeKey(nbKeys, i);
+        Random random = new Random();
+        ThrowingRunnable add = () -> {
+            String key = computeKey(nbKeys, random.nextInt());
             testee.store(createMail(key));
             expectedResult.add(key);
         };
 
-        Function<Integer, CheckedRunnable> remove = (Integer i) -> () -> {
-            String key = computeKey(nbKeys, i);
+        ThrowingRunnable remove = () -> {
+            String key = computeKey(nbKeys, random.nextInt());
             testee.remove(key);
             expectedResult.remove(key);
         };
 
-        int nbIterations = 1000;
-        Future.sequence(
-            Partition
-                .create(ImmutableMultimap.of(2, add, 6, remove))
-                .generateRandomStream()
-                .zipWithIndex()
-                .map(x -> x._1.apply(x._2))
-                .take(nbIterations)
-                .map(runnable -> io.vavr.concurrent.Future.run(executorService, runnable)))
-            .await();
+        DiscreteDistribution<ThrowingRunnable> distribution = DiscreteDistribution.create(
+            ImmutableList.of(
+                new DistributionEntry<>(add, 2),
+                new DistributionEntry<>(remove, 6)));
+
+        new ConcurrentTestRunner(threadCount, nbIterations,
+            (a, b) -> distribution.sample().run())
+            .run()
+            .awaitTermination(1, TimeUnit.MINUTES);
 
         assertThat(testee.list()).containsOnlyElementsOf(expectedResult);
     }
 
-    @NotNull
     default String computeKey(int nbKeys, Integer i) {
         int keyIndex = computeKeyIndex(nbKeys, i);
         return "mail" + keyIndex;
