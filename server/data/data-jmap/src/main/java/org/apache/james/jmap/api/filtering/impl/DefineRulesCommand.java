9f65b71d9ed7c701cc515229a17dade854931c99
Matthieu Baechler <matthieu@apache.org>
2018-08-22 16:08:23 +0200

    JIRA-XXX implementation of a FilteringManagement API using Event Sourcing

---
+++ server/data/data-jmap/src/main/java/org/apache/james/jmap/api/filtering/impl/DefineRulesCommand.java
@@ -0,0 +1,70 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.jmap.api.filtering.impl;
+
+import java.util.List;
+import java.util.Objects;
+
+import org.apache.james.core.User;
+import org.apache.james.eventsourcing.Command;
+import org.apache.james.jmap.api.filtering.Rule;
+
+import com.google.common.base.MoreObjects;
+
+public class DefineRulesCommand implements Command {
+
+    private final User user;
+    private final List<Rule> rules;
+
+    public DefineRulesCommand(User user, List<Rule> rules) {
+        this.user = user;
+        this.rules = rules;
+    }
+
+    public List<Rule> getRules() {
+        return rules;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DefineRulesCommand that = (DefineRulesCommand) o;
+        return Objects.equals(user, that.user) &&
+            Objects.equals(rules, that.rules);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, rules);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+            .add("user", user)
+            .add("rules", rules)
+            .toString();
+    }
+}
---
+++ server/data/data-jmap/src/test/java/org/apache/james/jmap/api/filtering/FilteringManagementContract.java
@@ -0,0 +1,78 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+package org.apache.james.jmap.api.filtering;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.apache.james.core.User;
+import org.junit.jupiter.api.Test;
+
+import com.google.common.collect.ImmutableList;
+
+public interface FilteringManagementContract {
+
+    FilteringManagement instanciateFilteringManagement();
+
+    @Test
+    default void listingRulesForUnknownUserShouldReturnEmptyList() {
+        User user = User.fromUsername("bart@simpson.cartoon");
+        assertThat(instanciateFilteringManagement().listRulesForUser(user)).isEmpty();
+    }
+
+    @Test
+    default void listingRulesShouldThrowWhenNullUser() {
+        User user = null;
+        assertThatThrownBy(() -> instanciateFilteringManagement().listRulesForUser(user)).isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    default void listingRulesShouldReturnDefinedRules() {
+        User user = User.fromUsername("bart@simpson.cartoon");
+        FilteringManagement testee = instanciateFilteringManagement();
+        testee.defineRulesForUser(user, ImmutableList.of(Rule.of("1"), Rule.of("2")));
+        assertThat(testee.listRulesForUser(user)).containsExactly(Rule.of("1"), Rule.of("2"));
+    }
+
+    @Test
+    default void listingRulesShouldReturnLastDefinedRules() {
+        User user = User.fromUsername("bart@simpson.cartoon");
+        FilteringManagement testee = instanciateFilteringManagement();
+        testee.defineRulesForUser(user, ImmutableList.of(Rule.of("1"), Rule.of("2")));
+        testee.defineRulesForUser(user, ImmutableList.of(Rule.of("2"), Rule.of("1")));
+        assertThat(testee.listRulesForUser(user)).containsExactly(Rule.of("2"), Rule.of("1"));
+    }
+
+    @Test
+    default void definingRulesShouldThrowWhenDuplicateRules() {
+        User user = User.fromUsername("bart@simpson.cartoon");
+        FilteringManagement testee = instanciateFilteringManagement();
+        assertThatThrownBy(() -> testee.defineRulesForUser(user, ImmutableList.of(Rule.of("1"), Rule.of("1"))))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    default void definingRulesShouldKeepOrdering() {
+        User user = User.fromUsername("bart@simpson.cartoon");
+        FilteringManagement testee = instanciateFilteringManagement();
+        testee.defineRulesForUser(user, ImmutableList.of(Rule.of("3"), Rule.of("2"), Rule.of("1")));
+        assertThat(testee.listRulesForUser(user)).containsExactly(Rule.of("3"), Rule.of("2"), Rule.of("1"));
+    }
+
+}
