ff012ec49f368325d608ef4e85f273c348b310fd
Benoit Tellier <btellier@linagora.com>
2018-09-10 11:12:51 +0700

    JAMES-2545 Move RabbitMQ tests and utils to backend package

--- server/queue/queue-rabbitmq/src/test/java/org/apache/james/queue/rabbitmq/RabbitMqMailQueueFactoryTest.java
+++ server/queue/queue-rabbitmq/src/test/java/org/apache/james/queue/rabbitmq/RabbitMqMailQueueFactoryTest.java
@@ -27,6 +27,7 @@ import java.util.concurrent.TimeoutException;
 import javax.mail.internet.MimeMessage;
 
 import org.apache.http.client.utils.URIBuilder;
+import org.apache.james.backend.mailqueue.DockerRabbitMQ;
 import org.apache.james.backends.cassandra.CassandraCluster;
 import org.apache.james.backends.cassandra.DockerCassandraExtension;
 import org.apache.james.backends.cassandra.init.configuration.CassandraConfiguration;
