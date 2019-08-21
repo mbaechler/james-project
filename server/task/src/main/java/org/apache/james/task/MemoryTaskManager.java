8acae321197638915cc8059a0600b5bc0406fb14
Matthieu Baechler <matthieu@apache.org>
2019-08-21 17:04:20 +0200

    JAMES-2873 Add Hostname into Created event

--- server/task/src/main/java/org/apache/james/task/MemoryTaskManager.java
+++ server/task/src/main/java/org/apache/james/task/MemoryTaskManager.java
@@ -29,6 +29,8 @@ import java.util.function.Consumer;
 
 import javax.annotation.PreDestroy;
 
+import org.apache.james.task.eventsourcing.Hostname;
+
 import com.github.steveash.guavate.Guavate;
 import com.google.common.collect.ImmutableList;
 import reactor.core.publisher.Flux;
@@ -78,22 +80,24 @@ public class MemoryTaskManager implements TaskManager {
         }
     }
 
-    public static final Duration AWAIT_POLLING_DURATION = Duration.ofMillis(500);
+    private static final Duration AWAIT_POLLING_DURATION = Duration.ofMillis(500);
     public static final Duration NOW = Duration.ZERO;
+
+    private final Hostname hostname;
     private final WorkQueue workQueue;
     private final TaskManagerWorker worker;
     private final ConcurrentHashMap<TaskId, TaskExecutionDetails> idToExecutionDetails;
 
-    public MemoryTaskManager() {
-
-        idToExecutionDetails = new ConcurrentHashMap<>();
-        worker = new SerialTaskManagerWorker(updater());
+    public MemoryTaskManager(Hostname hostname) {
+        this.hostname = hostname;
+        this.idToExecutionDetails = new ConcurrentHashMap<>();
+        this.worker = new SerialTaskManagerWorker(updater());
         workQueue = new MemoryWorkQueue(worker);
     }
 
     public TaskId submit(Task task) {
         TaskId taskId = TaskId.generateTaskId();
-        TaskExecutionDetails executionDetails = TaskExecutionDetails.from(task, taskId);
+        TaskExecutionDetails executionDetails = TaskExecutionDetails.from(task, taskId, hostname);
         idToExecutionDetails.put(taskId, executionDetails);
         workQueue.submit(new TaskWithId(taskId, task));
         return taskId;
