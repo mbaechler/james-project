2fa6481d290935f22dd6b723bf98c7baf9de10bf
Raphael Ouazana <raphael.ouazana@linagora.com>
2019-10-03 10:27:38 +0200

    fixup! bind DistributedTaskManager for Webadmin integration tests

--- server/container/guice/guice-common/src/main/java/org/apache/james/modules/CommonServicesModule.java
+++ server/container/guice/guice-common/src/main/java/org/apache/james/modules/CommonServicesModule.java
@@ -26,7 +26,6 @@ import org.apache.james.filesystem.api.FileSystem;
 import org.apache.james.filesystem.api.JamesDirectoriesProvider;
 import org.apache.james.modules.server.DNSServiceModule;
 import org.apache.james.modules.server.DropWizardMetricsModule;
-import org.apache.james.modules.server.TaskManagerModule;
 import org.apache.james.onami.lifecycle.PreDestroyModule;
 import org.apache.james.server.core.configuration.Configuration;
 import org.apache.james.server.core.configuration.ConfigurationProvider;
