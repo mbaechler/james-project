2e7111dfd17833a3fdae4ee54306381762098a73
Raphael Ouazana <raphael.ouazana@linagora.com>
2019-10-07 15:46:44 +0200

    fixup! Serialization integration test for ClearMailRepositoryTask

--- server/protocols/webadmin/webadmin-mailrepository/src/main/java/org/apache/james/webadmin/service/ClearMailRepositoryTaskDTO.java
+++ server/protocols/webadmin/webadmin-mailrepository/src/main/java/org/apache/james/webadmin/service/ClearMailRepositoryTaskDTO.java
@@ -19,7 +19,6 @@
 package org.apache.james.webadmin.service;
 
 import java.util.List;
-import java.util.function.Function;
 
 import org.apache.james.json.DTOModule;
 import org.apache.james.mailrepository.api.MailRepository;
