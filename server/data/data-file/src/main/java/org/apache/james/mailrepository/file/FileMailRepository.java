9b260c05dce2232d9670093afe6750cba890bff9
Benoit Tellier <btellier@linagora.com>
2019-11-14 10:22:55 +0700

    fixup! [Refactoring] update varialbe is always true

--- server/data/data-file/src/main/java/org/apache/james/mailrepository/file/FileMailRepository.java
+++ server/data/data-file/src/main/java/org/apache/james/mailrepository/file/FileMailRepository.java
@@ -244,8 +244,6 @@ public class FileMailRepository implements MailRepository, Configurable, Initial
                     // retrying to retrieve from a file we'll be overwriting.
                     saveStream = false;
                 }
-
-                // its an update
             }
         }
         if (saveStream) {
