d088fab3bec266b693666681c03070aaaa32eb0e
Rene Cordier <rcordier@linagora.com>
2020-05-20 11:06:27 +0700

    JAMES-3171 Port QuotaLoader to jmap-rfc8621

--- server/protocols/jmap-rfc-8621/src/main/scala/org/apache/james/jmap/utils/quotas/QuotaLoader.scala
+++ server/protocols/jmap-rfc-8621/src/main/scala/org/apache/james/jmap/utils/quotas/QuotaLoader.scala
@@ -1,4 +1,4 @@
-/** **************************************************************
+/****************************************************************
  * Licensed to the Apache Software Foundation (ASF) under one   *
  * or more contributor license agreements.  See the NOTICE file *
  * distributed with this work for additional information        *
@@ -6,25 +6,24 @@
  * to you under the Apache License, Version 2.0 (the            *
  * "License"); you may not use this file except in compliance   *
  * with the License.  You may obtain a copy of the License at   *
- * *
- * http://www.apache.org/licenses/LICENSE-2.0                 *
- * *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
  * Unless required by applicable law or agreed to in writing,   *
  * software distributed under the License is distributed on an  *
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
  * KIND, either express or implied.  See the License for the    *
  * specific language governing permissions and limitations      *
  * under the License.                                           *
- * ***************************************************************/
+ ****************************************************************/
 
-package org.apache.james.jmap.model
+package org.apache.james.jmap.utils.quotas
 
-import eu.timepit.refined.api.Refined
-import eu.timepit.refined.numeric.Interval.Closed
-import eu.timepit.refined.numeric.NonNegative
+import org.apache.james.jmap.mail.Quotas
+import org.apache.james.mailbox.exception.MailboxException
+import org.apache.james.mailbox.model.MailboxPath
 
-object UnsignedInt {
-//Unsigned int between [0, 2^53]
-  type UnsignedInt = Long Refined Closed[0L, 9007199254740992L]
-
+trait QuotaLoader {
+  @throws[MailboxException]
+  def getQuotas(mailboxPath: MailboxPath): Quotas
 }
